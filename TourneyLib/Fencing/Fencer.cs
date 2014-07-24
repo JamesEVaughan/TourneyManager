e1baf8cdd90173cf90d038275861fd847c93c369
JamesEVaughan <James.Earl.Vaughan@gmail.com>
2014-07-23 20:47:04 -0400

    Initial progress has be made on modeling of for individual fencers.
    Minor roadblock in deciding how to store and track individuals. Going to
    revise design.
    
    Next goal is to have a roster setup for fencers and to handle seeding.

---
+++ TourneyLib/Fencing/Fencer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TourneyLib.Fencing
+{
+    /// <summary>
+    /// Represents a single fencer in a tournament.
+    /// </summary>
+    class Fencer : IComparable
+    {
+        // Properties
+        /// <summary>
+        /// The fencer's first name.
+        /// </summary>
+        public string FirstName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+        /// <summary>
+        /// The fencer's last name.
+        /// </summary>
+        public string LastName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+        /// <summary>
+        /// The fencer's ranking. Used to determine initial seeding
+        /// </summary>
+        public Ranking Rank
+        {
+            get { return rank; }
+            set { rank = new Ranking(value); }
+        }
+        /// <summary>
+        /// The fencer's lot number, used for tiebreaking in initial seeding
+        /// </summary>
+        public int Lot
+        {
+            get { return lot; }
+            internal set { lot = value; }
+        }
+
+        // Constructors
+        public Fencer(string first, string last, string rnk)
+        {
+            fName = first;
+            lName = last;
+            rank = new Ranking(rnk);
+        }
+
+        // Methods
+
+
+        // IComparable
+        public int CompareTo(Object obj)
+        {
+            // If obj isn't valid, it goes first
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            // Now, try to make it a Fencer instance
+            Fencer othFen = obj as Fencer;
+            if (othFen == null)
+            {
+                // It's not a Fencer, throw an argument exception
+                throw new ArgumentException();
+            }
+
+            int result;
+            // If it is a fencer, do it by Rank...
+            result = rank.CompareTo(othFen.rank);
+            if (result != 0)
+            {
+                return result;
+            }
+            // If the Ranks are equal, break the tie with lots
+            else
+            {
+                return lot - othFen.lot;
+            }
+        }
+
+        // Fields
+        /// <summary>
+        /// The field behind the FirstName property.
+        /// </summary>
+        private string fName;
+        /// <summary>
+        /// The field behind the LastName property.
+        /// </summary>
+        private string lName;
+        /// <summary>
+        /// The field behind the Rank property
+        /// </summary>
+        private Ranking rank;
+        /// <summary>
+        /// The field behing the Lot property
+        /// </summary>
+        private int lot;
+    }
+}
---
+++ TourneyLib/Fencing/Ranking.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TourneyLib.Fencing
+{
+    /// <summary>
+    /// Represents a rating, used for initial seeding of a fencer.
+    /// </summary>
+    class Ranking : IComparable
+    {
+        public enum Classification
+        {
+            A = 1,
+            B,
+            C,
+            D,
+            E,
+            U
+        };
+
+        // Properties
+        public Classification Rating
+        {
+            get { return rating; }
+            internal set { rating = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            internal set { year = value; }
+        }
+
+        // Constructors/destructors
+        /// <summary>
+        /// Default contructor. Sets Ranking as lowest value
+        /// </summary>
+        public Ranking()
+        {
+            Rating = Classification.U;
+            Year = -1;
+        }
+        /// <summary>
+        /// Sets up Ranking based on a character and year.
+        /// </summary>
+        /// <param name="temp">String representing a ranking. Expect as a 3 
+        /// char string of a letter and two-digit year</param>
+        public Ranking(string temp)
+        {
+            char rt = temp[0];
+            int yr = Int32.Parse(temp.Substring(1));
+            switch (rt)
+            {
+                case 'a':
+                case 'A':
+                    Rating = Classification.A;
+                    break;
+                case 'b':
+                case 'B':
+                    Rating = Classification.B;
+                    break;
+                case 'c':
+                case 'C':
+                    Rating = Classification.C;
+                    break;
+                case 'd':
+                case 'D':
+                    Rating = Classification.D;
+                    break;
+                case 'e':
+                case 'E':
+                    Rating = Classification.E;
+                    break;
+                case 'u':
+                case 'U':
+                default:
+                    Rating = Classification.U;
+                    break;
+            }
+            if (rating == Classification.U)
+            {
+                year = -1;
+            }
+            else 
+            {
+                year = yr;
+            }
+        }
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="oth">Some other Ranking to be copied</param>
+        public Ranking(Ranking oth)
+        {
+            rating = oth.rating;
+            year = oth.year;
+        }
+
+        // Methods
+
+
+        // IComparable
+        public int CompareTo(Object obj)
+        {
+            // If obj isn't valid, it goes first
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            // Now, try to make it a Ranking instance
+            Ranking othRan = obj as Ranking;
+            if (othRan == null)
+            {
+                // It's not a Ranking, throw an argument exception
+                throw new ArgumentException();
+            }
+
+            // First, order by Rating
+            int result = (int)rating - (int)othRan.rating;
+            if (result != 0)
+            {
+                return result;
+            }
+            else if (rating == Classification.U)
+            {
+                return 0;
+            }
+            else
+            {
+                return year - othRan.year;
+            }
+        }
+
+        // Fields
+        private Classification rating;
+        private int year;
+    }
+}
